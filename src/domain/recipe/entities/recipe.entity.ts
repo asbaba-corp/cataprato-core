de899042f0f4c7d2c80db3ac6ea09dc52c081d60
danonel <daniel.luis@gerdau.com>
2023-08-03 14:00:35 -0300

    feat: add ingredients and creator to recipe entity

--- src/domain/recipe/entities/recipe.entity.ts
+++ src/domain/recipe/entities/recipe.entity.ts
@@ -1,8 +1,11 @@
 import { randomUUID } from 'crypto';
 import { CreateRecipeDto } from '../dto/create-recipe.dto';
+import { Ingredient } from '../../ingredients/entities/ingredient.entity';
 
 export class Recipe {
   id: string;
+  ingredients: Ingredient[];
+  creator: string;
   private constructor(private readonly recipe: CreateRecipeDto) {
     this.id = randomUUID();
   }
