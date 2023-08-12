5a95bdef34b4f8fed80692e79135e42da6f9fac5
Filipe Marron <52511902+filipezz@users.noreply.github.com>
2023-08-12 01:31:04 -0300

    Update allowed triggers

--- terraform.tf
+++ terraform.tf
@@ -63,7 +63,7 @@ module "lambda_function" {
   allowed_triggers = {
     APIGatewayAny = {
       service    = "apigateway"
-      source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:*/*/*/*"
+      source_arn = "arn:aws:execute-api:us-east-1:${data.aws_caller_identity.current.account_id}:${${tolist(data.aws_apigatewayv2_apis.cataprato)[0]}}/*/*"
     }
   }
 
@@ -142,4 +142,8 @@ module "s3_bucket" {
   versioning = {
     enabled = true
   }
+}
+
+data "aws_apigatewayv2_apis" "cataprato"{
+  protocol = "HTTP"
 }
