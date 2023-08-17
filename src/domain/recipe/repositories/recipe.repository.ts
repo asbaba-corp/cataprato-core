97761b970ca280cb58e45f5ca2a92e138ae17a9c
Daniel Luis <56178612+danonel@users.noreply.github.com>
2023-08-17 17:55:21 -0300

    feat desserialize  recipes (#51)
    * config: ensure all permisions to dynamodb
    
    * feat: add created at to recipe
    
    * feat: include createdAt on DTO
    
    * feat: desserialize recipes from dynamo

--- src/domain/recipe/repositories/recipe.repository.ts
+++ src/domain/recipe/repositories/recipe.repository.ts
@@ -14,9 +14,10 @@ interface DynamoDbRecipe {
   name: {
     S: string;
   };
-  ingredients: {
-    SS: string[];
-  };
+  ingredients: Set<string>;
+  created_at: {
+    S: string
+  }
 }
 
 @Injectable()
@@ -31,7 +32,7 @@ export class RecipeRepository {
           TableName: this.tableName,
         }),
       );
-      return result.Items;
+      return result.Items.map(this.deserializeRecipeFromDynamodbFormat);
     } catch (e) {
       console.error(e);
       throw new InternalServerErrorException();
@@ -77,4 +78,15 @@ export class RecipeRepository {
       }
     };
   }
+
+  private deserializeRecipeFromDynamodbFormat(dynamoRecipe: DynamoDbRecipe) {
+    return {
+        id: dynamoRecipe.id.S,
+        creator: dynamoRecipe.creator_id.S,
+        ingredients: [...dynamoRecipe.ingredients],
+        name: dynamoRecipe.name.S,
+        createdAt: dynamoRecipe.created_at
+          
+    };
+}
 }
